bce337d6a4b979749e5005c5d776fcfe50a1a7b1
Naufaldo <tagashik@gmail.com>
2023-05-19 16:59:43 +0700

    update delay time every 15 minute

--- sky.py
+++ sky.py
@@ -2,6 +2,7 @@ import subprocess
 from datetime import datetime
 import os
 from google.cloud import storage
+from time import sleep
 
 # Set the path to your JSON credentials file
 credentials_path = 'json/credentials.json'
@@ -18,22 +19,27 @@ subprocess.run(['gcloud', 'auth', 'activate-service-account', '--key-file', cred
 # Set the Google Cloud project ID
 subprocess.run(['gcloud', 'config', 'set', 'project', project_id])
 
-# Generate the current date and time
-current_datetime = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
 
-# Set the image file name using the current date and time
-image_file_name = f'{current_datetime}.jpg'
+while True:
+    # Generate the current date and time
+    current_datetime = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
 
-# Capture an image using fswebcam
-subprocess.run(['fswebcam', '-r', '1280x720', '--no-banner', image_file_name])
+    # Set the image file name using the current date and time
+    image_file_name = f'{current_datetime}.jpg'
 
-# Create a client for Google Cloud Storage
-client = storage.Client(project=project_id)
+    # Capture an image using fswebcam
+    subprocess.run(['fswebcam', '-r', '1280x720', '--no-banner', image_file_name])
 
-# Upload the image to Google Cloud Storage
-bucket = client.bucket(bucket_name)
-blob = bucket.blob(image_file_name)
-blob.upload_from_filename(image_file_name)
+    # Create a client for Google Cloud Storage
+    client = storage.Client(project=project_id)
 
-# Remove the local picture file
-os.remove(image_file_name)
+    # Upload the image to Google Cloud Storage
+    bucket = client.bucket(bucket_name)
+    blob = bucket.blob(image_file_name)
+    blob.upload_from_filename(image_file_name)
+
+    # Remove the local picture file
+    os.remove(image_file_name)
+
+    # Sleep for 15 minutes before capturing the next photo
+    sleep(900)  # 900 seconds = 15 minutes
